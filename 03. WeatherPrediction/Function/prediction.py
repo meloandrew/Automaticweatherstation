a7bb7fe771e1d2d7a619af14c1e7a060cfa5d89f
Leônidas do Nascimento <30960027+leonidasnascimento@users.noreply.github.com>
2020-10-18 15:31:51 -0300

    Update prediction.py

--- 03. WeatherPrediction/Function/prediction.py
+++ 03. WeatherPrediction/Function/prediction.py
@@ -1,8 +1,12 @@
+import pickle
+# import numpy as np
 class Predict():
     def __init__(self):
         pass
 
     def run(self, temperature: float, humidity: float, wind_velocity: float, pressure: float) -> int:
-        ## TODO: Dé, incluir as chamadas das funções preditivas
-        return 0
+        model = pickle.load(open("Function\modelo_rf.sav", "rb"))
+        arr = [[temperature, humidity, pressure, wind_velocity]]
+        predict: int = model.predict(arr)[0]
+        return predict.item()
     pass
